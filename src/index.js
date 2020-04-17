461e49d266545814b0e00d69b7a422f050cc9901
Michał Rożdżyński <drozdzu333@gmail.com>
2020-04-17 15:55:36 +0200

    initial commit. Install require tools

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// styles
+import './style/index.scss';
+
+// components
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
