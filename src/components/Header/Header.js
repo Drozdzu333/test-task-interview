ebd121524277ff187ae93890063c69399d87fd2f
Michał Rożdżyński <drozdzu333@gmail.com>
2020-04-26 12:43:54 +0200

    styled-footer-and-change-header-title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ import SocialMedia from '../SocialMedia/SocialMedia';
 const Header = () => (
   <header id="header">
     <div className="container">
-      <h1 className="header__h1">Create by Michał Rożdżyński</h1>
+      <h1 className="header__h1">Created by Michał Rożdżyński</h1>
       <SocialMedia />
     </div>
   </header>
